9e13ac64c126d777a4a01702eed6ccfd7f90713e
Jørgen Høst <jorgenhost96@gmail.com>
2025-12-08 20:29:45 +0100

    dbreg

---
+++ README.md
@@ -0,0 +1,12 @@
+# Large scale regression with `dbreg`
+
+Playing around with [`dbreg`](https://github.com/grantmcdermott/dbreg).
+
+Install `uv` and run:
+
+```bash
+uv sync
+uv run main.py
+```
+
+Then run `regz.rmd` in Rstudio. The speed of `dbreg` is kind of crazy - interacted vars/FEs a little complicated to deal with, though.
---
+++ main.py
@@ -0,0 +1,68 @@
+import os
+import urllib.request
+from tqdm import tqdm
+import polars as pl
+
+YEAR = 2018
+MONTH = 1
+END_YEAR = 2022
+END_MONTH = 12
+BASE_URL = 'https://d37ci6vzurychx.cloudfront.net'
+
+while YEAR <= END_YEAR:
+    year_dir = f'nyc-taxi/{YEAR}'
+    os.makedirs(year_dir, exist_ok=True)
+    
+    while MONTH <= 12:
+        if YEAR == END_YEAR and MONTH > END_MONTH:
+            break
+        
+        # Convert single-digit month to two-digit format
+        if len(str(MONTH)) == 1:
+            MONTH = '0' + str(MONTH)
+        else:
+            MONTH = str(MONTH)
+        
+        file_name = f'yellow_tripdata_{YEAR}-{MONTH}.parquet'
+        file_url = f'{BASE_URL}/trip-data/{file_name}'
+        print(f'Downloading {file_name}...')
+        
+        # Get the file size
+        response = urllib.request.urlopen(file_url)
+        file_size = int(response.info().get('Content-Length', -1))
+        
+        # Download the file with a progress bar
+        with open(os.path.join(year_dir, file_name), 'wb') as file, \
+             tqdm(unit='B', unit_scale=True, unit_divisor=1024, total=file_size) as progress:
+            block_size = 8192
+            while True:
+                buffer = response.read(block_size)
+                if not buffer:
+                    break
+                file.write(buffer)
+                progress.update(len(buffer))
+        
+        file_name_out = f'yellow_tripdata_{YEAR}-{MONTH}.pq'
+        (pl.scan_parquet(f'{year_dir}/{file_name}')
+            .with_columns(
+                month = pl.col("tpep_pickup_datetime").dt.month(), 
+                week = pl.col("tpep_pickup_datetime").dt.week(),
+                year = pl.col("tpep_pickup_datetime").dt.year(),
+                weekday = pl.col("tpep_pickup_datetime").dt.weekday()
+            )
+            .with_columns(
+                week_x_weekday = pl.col("week").cast(pl.Int16) * pl.col("weekday").cast(pl.Int16)  # Changed this line
+            )
+            .sink_parquet(f'{year_dir}/{file_name_out}')
+        )
+        
+        os.remove(f'{year_dir}/{file_name}')
+        
+        # Increment month
+        MONTH = int(MONTH) + 1
+    
+    # Increment year and reset month
+    YEAR += 1
+    MONTH = 1
+
+print('All files downloaded.')
